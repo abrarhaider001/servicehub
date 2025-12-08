e7d6b89d0234179a7362ee7daa1b92e93a977b6e
Abrar Haider <abrarhaider001@gmail.com>
2025-12-08 23:28:20 +0500

    first time app open check implemented

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -4,6 +4,7 @@ import 'package:servicehub/core/routes/app_routes.dart';
 import 'package:servicehub/core/utils/constants/image_strings.dart';
 import 'package:servicehub/core/utils/device/device_utility.dart';
 import 'package:servicehub/core/widgets/custom_background.dart';
+import 'package:servicehub/core/utils/local_storage/storage_utility.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -20,10 +21,11 @@ class _SplashState extends State<Splash> {
   }
 
   Future<void> _initFlow() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 2));
     if (!mounted) return;
-    Get.toNamed(AppRoutes.onBoarding);
-    
+    final flag = MyLocalStorage.instance().readData<dynamic>('isFirstTime') as bool?;
+    final isFirstTime = flag ?? true;
+    Get.offAllNamed(isFirstTime ? AppRoutes.onBoarding : AppRoutes.login);
   }
   @override
   Widget build(BuildContext context) {
